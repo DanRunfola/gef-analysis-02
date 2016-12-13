620d7c6613859d77e1649f668177fbdf0e692113
sgoodm <sgoodman@aiddata.org>
2016-12-13 16:44:01 -0500

    Fix bad col in old csvs and run merge of raw data

--- data_prep/build_merge.py
+++ data_prep/build_merge.py
@@ -1,6 +1,43 @@
+# merge raw data into single file
 
+import os
 import pandas as pd
 
+repo_dir = os.path.dirname(
+    os.path.dirname(os.path.realpath(__file__)))
+
+# land degradation (treatment)
+land_csv = "{0}/raw_data/landdegradation_treatment.csv".format(repo_dir)
+land_df = pd.read_csv(land_csv, quotechar='\"',
+                    na_values='', keep_default_na=False,
+                    encoding='utf-8')
+land_df['type'] = 'land'
+
+# multi focal area (treatment)
+mfa_csv = "{0}/raw_data/multifocalarea_treatment.csv".format(repo_dir)
+mfa_df = pd.read_csv(mfa_csv, quotechar='\"',
+                     na_values='', keep_default_na=False,
+                     encoding='utf-8')
+mfa_df['type'] = 'mfa'
+
+# programmatic (treatment)
+prog_csv = "{0}/raw_data/programmatic_treatment.csv".format(repo_dir)
+prog_df = pd.read_csv(prog_csv, quotechar='\"',
+                      na_values='', keep_default_na=False,
+                      encoding='utf-8')
+prog_df['type'] = 'prog'
+
+# programmatic (control)
+rand_csv = "{0}/raw_data/programmatic_control.csv".format(repo_dir)
+rand_df = pd.read_csv(rand_csv, quotechar='\"',
+                              na_values='', keep_default_na=False,
+                              encoding='utf-8')
+rand_df['type'] = 'rand'
 
 
-out_path = ""
+merged_df = pd.concat([land_df, mfa_df, prog_df, rand_df])
+merged_df['treatment'] = 0
+
+out_path = "{0}/data_prep/merged_data.csv".format(repo_dir)
+merged_df.to_csv(out_path, index=False, encoding='utf-8')
+
