eeb36f31f073b07150f2f1e0c83679c278040818
sgoodm <sgoodman@aiddata.org>
2016-12-13 16:51:22 -0500

    Add template for building each treatment csv

---
+++ data_prep/build_treatment_cases.py
@@ -0,0 +1,52 @@
+
+import pandas as pd
+
+
+# (M1) Programmatic w/ LD objectives;  Null Case Comparisons
+
+
+
+# (M2) Programmatic w/ Biodiversity objectives; Null Case Comparisons
+
+
+
+# (M3) Programmatic w/ LD objectives;  MFA w/ LD objectives
+
+
+
+# (M4) Programmatic w/ Biodiversity objectives; MFA w/ Biodiversity objectives
+
+
+
+# (M5) Programmatic multi-country w/ LD objectives;  Non-programmatic single-country w/ LD objectives
+
+
+
+# (M6) Programmatic multi-country w/ Biodiversity objectives; Non-programmatic single-country w/ Biodiversity objectives
+
+
+
+# (M7) Programmatic multi-agency w/ LD objectives;  Non-programmatic single-agency w/ LD objectives
+
+
+
+# (M8) Programmatic multi-agency w/ Biodiversity objectives; Non-programmatic single-agency w/ Biodiversity objectives
+
+
+
+# (M9) Programmatic multi-country w/ LD objectives;  Programmatic single-country w/ LD objectives
+
+
+
+# (M10) Programmatic multi-country w/ Biodiversity objectives; Programmatic single-country w/ Biodiversity objectives
+
+
+
+# (M11) Programmatic multi-agency w/ LD objectives;  Programmatic single-agency w/ LD objectives
+
+
+
+# (M12) Programmatic multi-agency w/ Biodiversity objectives; Programmatic single-agency w/ Biodiversity objectives
+
+
+
