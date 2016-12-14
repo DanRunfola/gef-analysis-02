3053e00c810b0c5c0fa6775e8fd8fa0a12a0c170
sgoodm <sgoodman@aiddata.org>
2016-12-14 10:18:22 -0500

    Add shps/csv for programmatic buffer centroids and merged with programmatic extracts so they have coordinate data

--- data_prep/build_merge.py
+++ data_prep/build_merge.py
@@ -27,12 +27,27 @@ prog_df = pd.read_csv(prog_csv, quotechar='\"',
                       encoding='utf-8')
 prog_df['type'] = 'prog'
 
+prog_coord_csv = "{0}/raw_data/programmatic_treatments_coords.csv".format(repo_dir)
+prog_coord_df = pd.read_csv(prog_coord_csv, quotechar='\"',
+                            na_values='', keep_default_na=False,
+                            encoding='utf-8')
+
+prog_df = prog_df.merge(prog_coord_df[['id', 'latitude', 'longitude']], on='id')
+
+
 # programmatic (control)
 rand_csv = "{0}/raw_data/programmatic_control.csv".format(repo_dir)
 rand_df = pd.read_csv(rand_csv, quotechar='\"',
-                              na_values='', keep_default_na=False,
-                              encoding='utf-8')
+                      na_values='', keep_default_na=False,
+                      encoding='utf-8')
 rand_df['type'] = 'rand'
+
+rand_coord_csv = "{0}/raw_data/programmatic_controls_coords.csv".format(repo_dir)
+rand_coord_df = pd.read_csv(rand_coord_csv, quotechar='\"',
+                            na_values='', keep_default_na=False,
+                            encoding='utf-8')
+
+rand_df = rand_df.merge(rand_coord_df[['id', 'latitude', 'longitude']], on='id')
 
 
 merged_df = pd.concat([land_df, mfa_df, prog_df, rand_df])
