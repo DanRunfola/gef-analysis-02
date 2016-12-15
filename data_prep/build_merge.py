7e63e23b80a1b1445f9b1d01f415a7c5d8ea63d5
sgoodm <sgoodman@aiddata.org>
2016-12-15 13:20:57 -0500

    Add biodiversity data to merge

--- data_prep/build_merge.py
+++ data_prep/build_merge.py
@@ -6,6 +6,14 @@ import pandas as pd
 repo_dir = os.path.dirname(
     os.path.dirname(os.path.realpath(__file__)))
 
+# biodiversity (treatment)
+bio_csv = "{0}/raw_data/biodiversity_treatment.csv".format(repo_dir)
+bio_df = pd.read_csv(bio_csv, quotechar='\"',
+                     na_values='', keep_default_na=False,
+                     encoding='utf-8')
+bio_df['type'] = 'bio'
+
+
 # land degradation (treatment)
 land_csv = "{0}/raw_data/landdegradation_treatment.csv".format(repo_dir)
 land_df = pd.read_csv(land_csv, quotechar='\"',
@@ -13,12 +21,14 @@ land_df = pd.read_csv(land_csv, quotechar='\"',
                     encoding='utf-8')
 land_df['type'] = 'land'
 
+
 # multi focal area (treatment)
 mfa_csv = "{0}/raw_data/multifocalarea_treatment.csv".format(repo_dir)
 mfa_df = pd.read_csv(mfa_csv, quotechar='\"',
                      na_values='', keep_default_na=False,
                      encoding='utf-8')
 mfa_df['type'] = 'mfa'
+
 
 # programmatic (treatment)
 prog_csv = "{0}/raw_data/programmatic_treatment.csv".format(repo_dir)
@@ -50,8 +60,12 @@ rand_coord_df = pd.read_csv(rand_coord_csv, quotechar='\"',
 rand_df = rand_df.merge(rand_coord_df[['id', 'latitude', 'longitude']], on='id')
 
 
-merged_df = pd.concat([land_df, mfa_df, prog_df, rand_df])
-merged_df['treatment'] = 0
+
+
+
+
+merged_df = pd.concat([bio_df, land_df, mfa_df, prog_df, rand_df])
+merged_df['treatment'] = -1
 
 out_path = "{0}/data_prep/merged_data.csv".format(repo_dir)
 merged_df.to_csv(out_path, index=False, encoding='utf-8')
